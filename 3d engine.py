d0a1394c30a08032b456defe6fe301bbf9f55e28
Gerome <quantaleprojects>
2022-08-23 17:59:27 +0200

    first commit

---
+++ 3d engine.py
@@ -0,0 +1,45 @@
+import os
+import time
+import pip
+from math import sin, cos, sqrt
+try:
+    import keyboard
+except:
+    print('Installing keybord module...')
+    pip.main(['install', "keyboard"])
+    try:
+        import keyboard
+    except:
+        print('Failed to install "keyboard" module,\ncheck your internet connection, make sure you have admin privilege\nor try : pip install keybord')
+        input()
+width,height = os.get_terminal_size()
+pixelBuffer = [' ']*(width*height-width)
+focalLengh = 1
+c=0
+
+def clear(char):
+    for i in range(width*height-width):
+        pixelBuffer[i] = char
+def draw():
+    print(''.join(pixelBuffer),end='')
+def putPixel(x,y,char):
+    if 1<x<width-3 and 1<y<height-2:
+        pixelBuffer[round(y)*width+round(x)] = char
+
+def projection(x,y,z):
+    nx = 2*x/width
+    ny = 2*y/height
+    nz = (2*z/100)
+    px = nx*focalLengh/nz
+    py = ny*focalLengh/nz
+    return (px+1)*width/2,(py+1)*height/2
+
+while True:
+    clear(' ')
+    c+=0.01
+    for i in range(-5,5):
+        for j in range(-5,5):
+            for k in range(1,6):
+                pos = projection(i,j,k+c)
+                putPixel(pos[0],pos[1],'#')
+    draw()
